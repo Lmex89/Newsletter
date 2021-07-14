b1031b1afe9975a21c1ef71035c2721438fb900b
Luis Mex <bjt.mex@gmail.com>
2021-07-13 19:09:15 -0500

    removing-venv

--- Newsletter/venv_newsletter/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/lmex89/Documents/Python/Workteam/Newsletter/Newsletter/venv_newsletter/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
